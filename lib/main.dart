1dd42abae732de3f216ad85e6f220279b4442f5d
rejaul <devrejaul.official@gmail.com>
2023-05-12 01:09:51 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "learn-dart",
-    home: Scaffold(
-      body: Center(child: Text("Hello World dart hi")),
-    ),
-  ));
+      title: "learn-dart",
+      home: Scaffold(
+        body: Center(
+          child: Text("hello dart"),
+        ),
+      )));
 }
